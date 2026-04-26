95877007e5dd24005c0ed11f4da95ed62ea3773a
Mohd Hamza <hamzamohd092007@gmail.com>
2026-04-26 10:08:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-app-dun-six-62.vercel.app",
     credentials: true
 }));
 
